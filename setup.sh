8966fad3c8541c6d607040aeb2ff6d3e1a07a292
zandtjuck <zanderination@gmail.com>
2021-09-13 06:57:42 +0700

    penambahan fitus setup cp httpd.conf

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ pkg up -y;
 pkg i php apache2 php-apache mariadb -y;
 cp php.conf $PREFIX/etc/apache2/conf.d/php.conf;
 echo "success cp php.conf to $PREFIX/etc/apache2/conf.d/php.conf";
+termux-setup-storage;
+mkdir /sdcard/htdocs;
+cp index.html /sdcard/htdocs;
+ln -s /sdcard/htdocs htdocs;
 
