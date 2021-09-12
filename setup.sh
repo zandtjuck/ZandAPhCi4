ab6d870760e7c2665c453ac3b5fa53a83f012bc8
zandtjuck <zanderination@gmail.com>
2021-09-13 06:43:58 +0700

    memberi fitur notifikasi telah tercopy php.conf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pkg up -y
-pkg i php apache2 php-apache mariadb -y
-cp php.conf $PREFIX/etc/apache2/conf.d/php.conf
+pkg up -y;
+pkg i php apache2 php-apache mariadb -y;
+cp php.conf $PREFIX/etc/apache2/conf.d/php.conf;
+echo "success cp php.conf to $PREFIX/etc/apache2/conf.d/php.conf";
 
